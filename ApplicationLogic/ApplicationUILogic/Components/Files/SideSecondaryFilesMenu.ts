d9c23925f92277f5d6fa8396a839b0e453f7a3e1
aartsiukouski <andrei.artsiukouski@zextras.com>
2022-08-02 13:52:22 +0300

    Create structure for SideSecondaryFilesMenu

---
+++ ApplicationLogic/ApplicationUILogic/Components/Files/SideSecondaryFilesMenu.ts
@@ -0,0 +1,23 @@
+import {BasePage, InheritedFields} from '../../Pages/BasePage';
+
+export class SideSecondaryFilesMenu extends BasePage {
+  constructor(page) {
+    super(page);
+  }
+
+  Containers = {
+    MainContainer: this.page.locator(InheritedFields.SideSecondaryDefaultBarLocator),
+  };
+
+  Tabs = {
+    Home: this.Containers.MainContainer.locator('"Home"'),
+    SharedWithMe: this.Containers.MainContainer.locator('"Shared with me"'),
+    Trash: this.Containers.MainContainer.locator('"Trash"'),
+    Filters: this.Containers.MainContainer.locator('"Filters"'),
+    Uploads: this.Containers.MainContainer.locator('"Uploads"'),
+  };
+
+  Buttons = {
+    CollapseTrash: this.Containers.MainContainer.locator(''),
+  };
+}
