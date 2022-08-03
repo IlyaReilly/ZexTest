fcd9cebd9bf31f423a8909d6095f97a6c3c41f67
andrei.artsiukhouski <andreia@ttro.com>
2022-08-03 10:03:07 +0300

    Calendar and chats tests

--- ApplicationLogic/ApplicationUILogic/Components/Chats/SideSecondaryChatsMenu.ts
+++ ApplicationLogic/ApplicationUILogic/Components/Chats/SideSecondaryChatsMenu.ts
@@ -1,37 +1,37 @@
-import {BasePage, InheritedFields} from '../../Pages/BasePage';
+import {BasePage} from '../../Pages/BasePage';
 
 export class SideSecondaryChatsMenu extends BasePage {
-    Containers = {
-        MainContainer: this.page.locator('.cpSZnV'),
-    };
+  Containers = {
+    MainContainer: this.page.locator('.cpSZnV'),
+  };
 
-    Buttons = {
-        CreateVirtualRoom: this.Containers.MainContainer.locator('"CREATE VIRTUAL ROOM"'),
-        PersonalVirtualRoom: this.Containers.MainContainer.locator('"PERSONAL VIRTUAL ROOM"'),
-        CopyLink: this.Containers.MainContainer.locator('"COPY LINK"'),
-    };
+  Buttons = {
+    CreateVirtualRoom: this.Containers.MainContainer.locator('"CREATE VIRTUAL ROOM"'),
+    PersonalVirtualRoom: this.Containers.MainContainer.locator('"PERSONAL VIRTUAL ROOM"'),
+    CopyLink: this.Containers.MainContainer.locator('"COPY LINK"'),
+  };
 
-    Tabs = {
-        Chats: this.Containers.MainContainer.locator('"Chats"'),
-        Spaces: this.Containers.MainContainer.locator('"Spaces"'),
-        VirtualRooms: this.Containers.MainContainer.locator('"Virtual Rooms"'),
-    }
+  Tabs = {
+    Chats: this.Containers.MainContainer.locator('"Chats"'),
+    Spaces: this.Containers.MainContainer.locator('"Spaces"'),
+    VirtualRooms: this.Containers.MainContainer.locator('"Virtual Rooms"'),
+  };
 
-    Textboxes = {
-        FilterChatsList: this.Containers.MainContainer.locator('[name="Filter chats list"]')
-    }
+  Textboxes = {
+    FilterChatsList: this.Containers.MainContainer.locator('[name="Filter chats list"]'),
+  };
 
-    Elements = {
-        ConversationsListItem : this.Containers.MainContainer.locator('.heROSq')
-    }
+  Elements = {
+    ConversationsListItem: this.Containers.MainContainer.locator('.heROSq'),
+  };
 
-    constructor(page){
-        super(page);
-    }
+  constructor(page) {
+    super(page);
+  }
 
-    OpenTab = {
-        Chats: async () => await this.Tabs.Chats.click(),
-        Spaces: async () => await this.Tabs.Spaces.click(),
-        VirtualRooms: async () => await this.Tabs.VirtualRooms.click(),
-    }
+  OpenTab = {
+    Chats: async () => await this.Tabs.Chats.click(),
+    Spaces: async () => await this.Tabs.Spaces.click(),
+    VirtualRooms: async () => await this.Tabs.VirtualRooms.click(),
+  };
 }
--- ApplicationLogic/ApplicationUILogic/Pages/PageManager.ts
+++ ApplicationLogic/ApplicationUILogic/Pages/PageManager.ts
@@ -7,43 +7,43 @@ import {SideSecondaryMailMenu} from '../Components/Mails/SideSecondaryMailMenu';
 import {SideSecondaryCalendarMenu} from '../Components/Calendars/SideSecondaryCalendarMenu';
 import {MailsList} from '../Components/Mails/MailsList';
 import {Calendar} from '../Components/Calendars/Calendar';
-import { SideSecondaryChatsMenu } from '../Components/Chats/SideSecondaryChatsMenu';
-import { NewChatsItem } from '../Components/Chats/NewChatsItem';
+import {SideSecondaryChatsMenu} from '../Components/Chats/SideSecondaryChatsMenu';
+import {NewChatsItem} from '../Components/Chats/NewChatsItem';
 
 export class PageManager {
-    //#region Pages
-    async getLoginPage(page){
-        return await new LoginPage(page);
-    }
-    //#endregion
+  // #region Pages
+  async getLoginPage(page) {
+    return await new LoginPage(page);
+  }
+  // #endregion
 
-    //#region Main Components
+  // #region Main Components
   async getHeaderMenuComponent(page) {
     return await new HeaderMenu(page);
   }
 
-    async getSideMenuComponent(page){
-        return await new SideMenu(page);
-    }
-    //#endregion
+  async getSideMenuComponent(page) {
+    return await new SideMenu(page);
+  }
+  // #endregion
 
-    //#region Mail Components
+  // #region Mail Components
   async getNewMailComponent(page) {
     return await new NewMail(page);
   }
 
-    async getSideSecondaryMailMenuComponent(page){
-        return await new SideSecondaryMailMenu(page);
+  async getSideSecondaryMailMenuComponent(page) {
+    return await new SideSecondaryMailMenu(page);
   }
 
-    async getMailsListComponent(page){
-        return await new MailsList(page);
+  async getMailsListComponent(page) {
+    return await new MailsList(page);
   }
-    //#endregion
+  // #endregion
 
-    //#region Calendar Components
-    async getNewAppointmentComponent(page){
-        return await new NewAppointment(page);
+  // #region Calendar Components
+  async getNewAppointmentComponent(page) {
+    return await new NewAppointment(page);
   }
 
   async getSideSecondaryCalendarMenuComponent(page) {
@@ -53,15 +53,15 @@ export class PageManager {
   async getCalendarComponent(page) {
     return await new Calendar(page);
   }
-    //#endregion
+  // #endregion
 
-    //#region Calendar Components
-    async getNewChatsItemComponent(page){
-        return await new NewChatsItem(page);
-    }
+  // #region Calendar Components
+  async getNewChatsItemComponent(page) {
+    return await new NewChatsItem(page);
+  }
 
-    async getSideSecondaryChatsMenuComponent(page){
-        return await new SideSecondaryChatsMenu(page);
-    }
-    //#endregion
+  async getSideSecondaryChatsMenuComponent(page) {
+    return await new SideSecondaryChatsMenu(page);
+  }
+  // #endregion
 }
