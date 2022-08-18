7c17b3666173a77a1ac759ec57fd62ebd6897a74
andrei.artsiukhouski <andreia@ttro.com>
2022-08-18 22:25:59 +0300

    Fix mail 2 elements exception

--- ApplicationLogic/ApplicationUILogic/Components/Mails/SideSecondaryMailMenu.ts
+++ ApplicationLogic/ApplicationUILogic/Components/Mails/SideSecondaryMailMenu.ts
@@ -6,7 +6,7 @@ export class SideSecondaryMailMenu extends BasePage {
   };
 
   Buttons = {
-    OpenHideMailFolders: this.Containers.MainContainer.locator('.cLLOPN'),
+    OpenHideMailFolders: this.Containers.MainContainer.locator('.cLLOPN >> nth=0'),
   };
 
   MailFolders = {
