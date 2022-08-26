c6730cc377890c6938178028e1022e5d20d20373
aartsiukouski <andrei.artsiukouski@zextras.com>
2022-08-26 11:24:44 +0300

    Fix Calendar Tests: click on an appointment

--- ApplicationLogic/ApplicationUILogic/Components/Calendars/Calendar.ts
+++ ApplicationLogic/ApplicationUILogic/Components/Calendars/Calendar.ts
@@ -36,8 +36,8 @@ export class Calendar extends BasePage {
     const appointmentElement = await this.page.waitForSelector(appointmentLocator._selector, {state: 'attached'});
     await appointmentElement.evaluate((el) => el.style.display = 'inline');
     await appointmentElement.evaluate((el) => el.style.visibility = 'visible');
-    await appointmentElement.evaluate((el) => el.click());
-    // await this.Elements.Appointment.locator(`"${appointmentTitle}"`).click();
+    await this.Elements.Appointment.locator(`"${appointmentTitle}"`).click();
+    // await appointmentElement.evaluate((el) => el.click());
   }
 
   async MoveAppointmentToTrash(appointmentTitle) {
