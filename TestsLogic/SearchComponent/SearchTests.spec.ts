bfe415fcea66636fab4e6654e363d41728b2c723
aartsiukouski <andrei.artsiukouski@zextras.com>
2022-09-07 19:13:23 +0300

    Fix: Transfer Test Files into Assets folder

--- TestsLogic/SearchComponent/SearchTests.spec.ts
+++ TestsLogic/SearchComponent/SearchTests.spec.ts
@@ -72,8 +72,8 @@ test.describe('Search tests', async () => {
     const templateFileName = 'fileForSearch.png';
     const fileName = uniquePrefix + 'fileForSearch';
     const fileNameFull = fileName + '.png';
-    const filePathSrc = path.resolve("./TestData/", templateFileName);
-    const filePathDest = path.resolve("./TestData/", fileNameFull);
+    const filePathSrc = path.resolve("./TestData/Assets/", templateFileName);
+    const filePathDest = path.resolve("./TestData/Assets/", fileNameFull);
     fs.copyFileSync(filePathSrc, filePathDest);
 
     try {
