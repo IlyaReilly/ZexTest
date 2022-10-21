b6f325323722de06abf8f95cbaa37caa7c8a4ce9
ArtsemKanaval <artsem.kanaval@zextras.com>
2022-10-21 14:21:16 +0300

    Edit after main changes

--- TestsLogic/ChatsComponent/VirtualRoomsTests.spec.ts
+++ TestsLogic/ChatsComponent/VirtualRoomsTests.spec.ts
@@ -19,14 +19,7 @@ test.describe('Virtual Rooms tests', async () => {
   async function CleanConversationsPanel({apiManager}) {
     const conversations = await apiManager.chatsAPI.GetConversations();
     await Promise.all(conversations.map(async (conversation) => {
-      return apiManager.chatsAPI.DeleteConversation(conversation.id);
-    }));
-    await Promise.all(conversations.map(async (conversation) => {
-      const users = await apiManager.chatsAPI.GetUsers();
-      return apiManager.chatsAPI.KickOffUser(conversation.id, users);
-    }));
-    await Promise.all(conversations.map(async (conversation) => {
-      return apiManager.chatsAPI.DeleteGroup(conversation.id);
+      return apiManager.deleteChatsAPI.DeleteGroup(conversation.id);
     }));
   };
 
@@ -35,7 +28,7 @@ test.describe('Virtual Rooms tests', async () => {
     await pageManager.newVirtualRoomsModal.CreateVirtualRoom(title);
   }
 
-  test('Create virtual room. Virtual room should be in Virtual Rooms Tab.', async ({pageManager}) => {
+  test('Create virtual room. Virtual room should be visible in Virtual Rooms Tab.', async ({pageManager}) => {
     test.fail();
     await CreateVirtualRoom({pageManager}, virtualRoomTitle);
     await pageManager.sideSecondaryChatsMenu.OpenTab.VirtualRooms();
@@ -43,7 +36,7 @@ test.describe('Virtual Rooms tests', async () => {
   });
 
   test('Copy Virtual room link. Virtual room link should be in clipboard.', async ({page, pageManager, apiManager}) => {
-    await apiManager.chatsAPI.CreateVirtualRoom(virtualRoomTitle);
+    await apiManager.createChatsAPI.CreateVirtualRoom(virtualRoomTitle);
     await pageManager.sideSecondaryChatsMenu.OpenTab.VirtualRooms();
     await pageManager.sideSecondaryChatsMenu.Elements.ConversationsItem.click();
     await pageManager.virtualRoomField.Buttons.VirtualRoomLink.click();
