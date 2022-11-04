8aa082526ab6a063856e5318b8f08d1315a1557d
ArtsemKanaval <artsem.kanaval@zextras.com>
2022-11-04 18:23:04 +0300

    Add TC605-606 tests

--- TestsLogic/ContactsComponent/ContactsTests.spec.ts
+++ TestsLogic/ContactsComponent/ContactsTests.spec.ts
@@ -6,15 +6,18 @@ test.describe('Contacts tests', async () => {
   let mailSubject;
   let mailBody;
   let firstName;
+  let newFirstName;
   let lastName;
   let email;
 
-  test.beforeEach(async () => {
+  test.beforeEach(async ({pageManager}) => {
     firstName = BaseTest.dateTimePrefix();
+    newFirstName = BaseTest.dateTimePrefix() + ' New';
     lastName = BaseTest.dateTimePrefix() + 'LName';
     email = BaseTest.dateTimePrefix() + '@test.com';
     mailSubject = BaseTest.dateTimePrefix() + ' Autotest Mail Subject';
     mailBody = BaseTest.dateTimePrefix() + ' Autotest Mail Body';
+    await pageManager.sideMenu.OpenMenuTab(pageManager.sideMenu.SideMenuTabs.Contacts);
   });
 
   test.afterEach(async ({page, apiManager}) => {
@@ -23,16 +26,14 @@ test.describe('Contacts tests', async () => {
     await page.close();
   });
 
-  test('Open contacts tab. Contacts folder options should be visible', async ({pageManager}) => {
-    await pageManager.sideMenu.OpenMenuTab(pageManager.sideMenu.SideMenuTabs.Contacts);
+  test('TC601. Open contacts tab. Contacts folder options should be visible', async ({pageManager}) => {
     await expect(pageManager.sideSecondaryContactsMenu.ContactAddressBooks.Contacts, 'Contacts tab should be presented').toBeVisible();
     await expect(pageManager.sideSecondaryContactsMenu.ContactAddressBooks.EmailedContacts, 'Emailed contacts tab should be presented').toBeVisible();
     await expect(pageManager.sideSecondaryContactsMenu.ContactAddressBooks.Trash, 'Trash tab should be presented').toBeVisible();
   });
 
-  test('Add new contact. New contact appears in contacts chapter', async ({page, pageManager}) => {
+  test('TC602. Add new contact. New contact appears in Contacts folder', async ({page, pageManager}) => {
     BaseTest.doubleTimeout();
-    await pageManager.sideMenu.OpenMenuTab(pageManager.sideMenu.SideMenuTabs.Contacts);
     await pageManager.headerMenu.Buttons.NewItem.click();
     await pageManager.newContact.CreateNewContact(firstName, lastName, email);
     const elementHandle = await page.$(InheritedFields.NewItemDefaultContainerLocator);
@@ -40,22 +41,39 @@ test.describe('Contacts tests', async () => {
     await expect(pageManager.contactsList.Containers.ContactsListContainer.locator(`"${email}"`), 'The e-mail address of a new contact is visible in Contacts list').toBeVisible();
   });
 
-  test('Emailed contact. New email reciever appears in emailed contact chapter', async ({page, pageManager, apiManager}) => {
+  test('TC603. Emailed contact. New email reciever appears in emailed Contacts folder', async ({pageManager, apiManager}) => {
     await apiManager.createMailsAPI.SendMsgRequest(mailSubject, BaseTest.userForLogin.login, email, mailBody);
-    await pageManager.sideMenu.OpenMenuTab(pageManager.sideMenu.SideMenuTabs.Contacts);
-    await pageManager.sideSecondaryContactsMenu.ContactAddressBooks.EmailedContacts.click();    
+    await pageManager.sideSecondaryContactsMenu.ContactAddressBooks.EmailedContacts.click();
     await expect(pageManager.contactsList.Containers.ContactsListContainer.locator(`"${email}"`), 'The e-mail address of a new contact is visible in Emailed contacts list').toBeVisible();
   });
 
-  test('Delete contact. Contact appears in trash chapter', async ({page, pageManager, apiManager}) => {
+  test('TC604. Delete contact. Contact appears in Trash folder', async ({page, pageManager, apiManager}) => {
     BaseTest.doubleTimeout();
     await apiManager.createContactsAPI.CreateContact(firstName, BaseTest.userForLogin.login);
-    await pageManager.sideMenu.OpenMenuTab(pageManager.sideMenu.SideMenuTabs.Contacts);
-    await pageManager.contactsList.Containers.ContactsListContainer.locator(`"${BaseTest.userForLogin.login}"`).first().click();
-    await pageManager.contactsList.DeleteContact();
+    await pageManager.contactsList.SelectContactContextMenuOption.Delete(BaseTest.userForLogin.login);
     await page.reload();
-    await pageManager.sideSecondaryContactsMenu.ContactAddressBooks.Trash.click();    
+    await pageManager.sideSecondaryContactsMenu.ContactAddressBooks.Trash.click();
     await expect(pageManager.contactsList.Containers.ContactsListContainer.locator(`"${firstName}"`), 'The first name of a new contact is visible in Trash contacts list').toBeVisible();
+  });
+
+  test('TC605. Delete contact permanently. Contact disappears from Trash folder', async ({pageManager, apiManager}) => {
+    BaseTest.doubleTimeout();
+    const contactId = await apiManager.createContactsAPI.CreateContact(firstName, BaseTest.userForLogin.login);
+    await apiManager.deleteContactsAPI.DeleteContactsById(contactId, BaseTest.userForLogin.login);
+    await pageManager.sideSecondaryContactsMenu.ContactAddressBooks.Trash.click();
+    await pageManager.contactsList.SelectContactContextMenuOption.DeletePermanently(BaseTest.userForLogin.login);
+    await pageManager.deleteContactPermanentlyModal.Buttons.DeletePermanently.click();
+    await expect(pageManager.contactsList.Containers.ContactsListContainer.locator(`"${firstName}"`), 'The first name of a new contact is not visible in Trash contacts list').not.toBeVisible();
+  });
+
+  test('TC606. Edit contact data. Edited contact data is visible in Contacts folder', async ({pageManager, apiManager}) => {
+    BaseTest.doubleTimeout();
+    await apiManager.createContactsAPI.CreateContact(firstName, BaseTest.userForLogin.login);
+    await pageManager.contactsList.Containers.ContactsListContainer.locator(`"${BaseTest.userForLogin.login}"`).first().click();
+    await pageManager.contactDetails.ContactOptions.Edit.click();
+    await pageManager.contactDetails.EditContactView.FirstName.fill(newFirstName);
+    await pageManager.contactDetails.EditContactView.Save.click();
+    await expect(pageManager.contactsList.Containers.ContactsListContainer.locator(`"${newFirstName}"`), 'The edited last name of contact is visible in Contacts list').toBeVisible();
   });
 });
 
