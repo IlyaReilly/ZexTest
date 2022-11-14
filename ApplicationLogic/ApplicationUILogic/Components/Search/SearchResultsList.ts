15c318413c9145ea453b01776f71e314a081869e
ArtsemKanaval <artsem.kanaval@zextras.com>
2022-11-14 15:30:05 +0300

    Fix some tests

--- ApplicationLogic/ApplicationUILogic/Components/Search/SearchResultsList.ts
+++ ApplicationLogic/ApplicationUILogic/Components/Search/SearchResultsList.ts
@@ -9,14 +9,14 @@ export class SearchResultsList extends BasePage {
     MainContainerMail: this.page.locator('.ereURg'),
     MainContainerContacts: this.page.locator('.dLDkZK'),
     MainContainerFiles: this.page.locator('.gcSrJU'),
-    MainContainerAppointments: this.page.locator('.iJzCtl'),
+    MainContainerAppointments: this.page.locator('.ereURg'),
   };
 
   Elements = {
     SearchResultMail: this.Containers.MainContainerMail.locator('.fbCbGB'),
     SearchResultContacts: this.Containers.MainContainerContacts.locator('.vEXXw'),
     SearchResultFiles: this.Containers.MainContainerFiles.locator('.cNpbyA'),
-    SearchResultAppointments: this.Containers.MainContainerAppointments.locator('.jTMZGq'),
+    SearchResultAppointments: this.Containers.MainContainerAppointments.locator('.hPLMBr'),
     AdvancedFilters: this.Containers.MainContainerMail.locator('"Advanced Filters"'),
   };
 }
--- TestsLogic/FoldersComponent/FoldersTest.spec.ts
+++ TestsLogic/FoldersComponent/FoldersTest.spec.ts
@@ -81,7 +81,6 @@ test.describe('Folders tests', async () => {
     const mailId = await apiManager.createMailsAPI.SendMsgRequest(mailSubject, BaseTest.userForLogin.login, BaseTest.secondUser.login, mailBody);
     await apiManager.mailsAPI.MoveMailToFolder(mailId, BaseTest.userForLogin.login, folderId);
     await OpenSentSubFolderContextMenu({pageManager});
-    await pageManager.mailsList.Elements.Letter.waitFor();
     await pageManager.sideSecondaryMailMenu.SelectMailFolderOption.WipeFolder();
     await pageManager.wipeFolderModal.WipeNewFolder();
     await pageManager.sideSecondaryMailMenu.OpenSubFolder(folderName);
