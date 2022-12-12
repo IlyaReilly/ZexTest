0ed1df2d741702b0e4110c854ef17e20976cb5a7
aartsiukouski <andrei.artsiukouski@zextras.com>
2022-12-12 21:00:01 +0300

    Add TC232-235 tests

--- ApplicationLogic/ApplicationAPILogic/Mails/CreateMailsAPI.ts
+++ ApplicationLogic/ApplicationAPILogic/Mails/CreateMailsAPI.ts
@@ -5,11 +5,18 @@ export class CreateMailsAPI extends BaseAPI {
     super(page);
   };
 
-  async SendMsgRequest(subject, from, to, mailBody) {
+  async SendMsgRequest(subject, from, to, mailBody, to2) {
     let id = '';
-    const response = await this.page.request.post(`${this.soapServiceUrl}${this.sendMsgRequest}`, {
-      data: this.FormingMsgRequestBody(this.sendMsgRequest, subject, from, to, mailBody),
-    });
+    let response;
+    if (!to2) {
+      response = await this.page.request.post(`${this.soapServiceUrl}${this.sendMsgRequest}`, {
+        data: this.FormingMsgRequestBody(this.sendMsgRequest, subject, from, to, mailBody),
+      });
+    } else {
+      response = await this.page.request.post(`${this.soapServiceUrl}${this.sendMsgRequest}`, {
+        data: this.FormingMsgRequestBodyToMultipleRecipients(this.sendMsgRequest, subject, from, to, mailBody, to2),
+      });
+    };
 
     const body = await this.GetResponseBody(response);
     if (body.Body.SendMsgResponse.m) {
@@ -66,6 +73,46 @@ export class CreateMailsAPI extends BaseAPI {
             e: [
               {t: 'f', a: from, d: ''},
               {t: 't', a: to},
+            ],
+            mp: [
+              {
+                ct: 'multipart/alternative',
+                mp: [
+                  {ct: 'text/html', body: true, content: {_content: `<p>${body}</p>`}},
+                  {ct: 'text/plain', content: {_content: body}},
+                ],
+              },
+            ],
+          },
+        },
+      },
+      Header: {
+        context: {
+          _jsns: 'urn:zimbra',
+          notify: {seq: 5},
+          session: {id: '11056', _content: '11056'},
+          account: {by: 'name', _content: from},
+          userAgent: {
+            name: 'CarbonioWebClient - Chrome 103.0.0.0 (Windows)',
+            version: '22.6.1_ZEXTRAS_202206 agent 20220621-1442 FOSS',
+          },
+        },
+      },
+    };
+  };
+
+  FormingMsgRequestBodyToMultipleRecipients(requestType, subject, from, to, body, to2) {
+    return {
+      Body: {
+        [requestType]: {
+          _jsns: 'urn:zimbraMail',
+          m: {
+            attach: {mp: []},
+            su: {_content: subject},
+            e: [
+              {t: 'f', a: from, d: ''},
+              {t: 't', a: to},
+              {t: 't', a: to2},
             ],
             mp: [
               {
