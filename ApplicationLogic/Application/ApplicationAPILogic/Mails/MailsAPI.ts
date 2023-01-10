6b0855342e748aa55ed7ec740ddb7a24b9aa28fb
aartsiukouski <andrei.artsiukouski@zextras.com>
2023-01-10 13:56:57 +0300

    Refactoring

--- ApplicationLogic/Application/ApplicationAPILogic/Mails/MailsAPI.ts
+++ ApplicationLogic/Application/ApplicationAPILogic/Mails/MailsAPI.ts
@@ -62,6 +62,7 @@ export class MailsAPI extends BaseAPI {
       },
     });
   };
+
   async DeleteMailViaAPI({apiManager}) {
     const mailIds = await this.getMailIds(BaseTest.userForLogin.login);
     await Promise.all(mailIds.map(async (id) => await this.ItemActionRequest(apiManager.mailsAPI.ActionRequestTypes.delete, id, BaseTest.userForLogin.login)));
