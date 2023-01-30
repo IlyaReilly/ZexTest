76588b6c64d29f8b5aca9dc2af6eba67f5e5bf5f
aartsiukouski <andrei.artsiukouski@zextras.com>
2023-01-30 16:58:53 +0300

    Add TC202-203

--- ApplicationLogic/Admin/AdminUILogic/Pages/AdminPageManager.ts
+++ ApplicationLogic/Admin/AdminUILogic/Pages/AdminPageManager.ts
@@ -1,15 +1,18 @@
 import {Page} from '@playwright/test';
 import {AdminHeaderMenu} from '../Components/AdminHeaderMenu';
 import {AdminSideMenu} from '../Components/AdminSideMenu';
+import {Dashboard} from '../Components//Dashboard/Dashboard';
 
 export class AdminPageManager {
   page: Page;
   adminHeaderMenu;
   adminSideMenu;
+  dashboard;
 
   constructor(page) {
     this.page = page;
     this.adminHeaderMenu = new AdminHeaderMenu(page);
     this.adminSideMenu = new AdminSideMenu(page);
+    this.dashboard = new Dashboard(page);
   };
 }
--- TestsLogic/AdminUITests/DashboardComponent/DashboardTests.spec.ts
+++ TestsLogic/AdminUITests/DashboardComponent/DashboardTests.spec.ts
@@ -1,14 +1,29 @@
 import {test} from '../../BaseTest';
 import {expect} from '@playwright/test';
 
-// Only architecture for admin tests
 test.describe('Admin. Dashboard tests.', async () => {
-  test.afterEach(async ({page}) => {
-    await page.close();
+  test.afterEach(async ({adminPageManager}) => {
+    await adminPageManager.adminSideMenu.OpenMenuTab(adminPageManager.adminSideMenu.SideMenuTabs.Dashboard);
   });
 
-  test('ATC201. Open Dashboard Tab. Dashboard path should be visible', async ({adminPageManager}) => {
-    await adminPageManager.adminSideMenu.OpenMenuTab(adminPageManager.adminSideMenu.SideMenuTabs.Dashboard);
-    await expect(adminPageManager.adminHeaderMenu.Containers.PathContainer).toHaveText('Home');
+  test.afterEach(async ({adminPage}) => {
+    await adminPage.close();
+  });
+
+  test('ATC201. Open Dashboard Tab. All fields should be visible', async ({adminPageManager}) => {
+    await expect(adminPageManager.dashboard.Fields.WelcomeMessage).toBeVisible();
+    await expect(adminPageManager.dashboard.Fields.QuickAccess).toBeVisible();
+    await expect(adminPageManager.dashboard.Fields.YourNotifications).toBeVisible();
+    await expect(adminPageManager.dashboard.Fields.ServersList).toBeVisible();
+  });
+
+  test('ATC202. Open Accounts list via Quick Access. Account list path should be visible', async ({adminPageManager}) => {
+    await adminPageManager.dashboard.Buttons.OpenAccounts.click();
+    await expect(adminPageManager.adminHeaderMenu.Containers.PathContainer).toHaveText('Home/Domains/Accounts');
+  });
+
+  test('ATC203. Open Mailing list via Quick Access. Mailing list path should be visible', async ({adminPageManager}) => {
+    await adminPageManager.dashboard.Buttons.OpenMailingList.click();
+    await expect(adminPageManager.adminHeaderMenu.Containers.PathContainer).toHaveText('Home/Domains/Mailing List');
   });
 });
